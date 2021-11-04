f8fefcc11ec38762686a9b841d5a7f011a9066b6
Moira Leon <moira.leon2@gmail.com>
2021-11-04 17:58:31 -0600

    first attempt

--- src/com/javapackage/Main.java
+++ src/com/javapackage/Main.java
@@ -1,8 +1,40 @@
 package com.javapackage;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        var site1 = CompletableFuture.supplyAsync(()->{
+            return 1;
+        });
+
+        var site2 = CompletableFuture.supplyAsync(()->{
+            return 2;
+        });
+
+        var site3 = CompletableFuture.supplyAsync(()->{
+            return 3;
+        });
+
+        var all = CompletableFuture.allOf(site1,site2,site3);
+        all.thenRun(()->{
+
+            try {
+                var firstResult = site1.get();
+                var secondResult = site2.get();
+                var thirdResult = site3.get();
+                System.out.println(firstResult);
+                System.out.println(secondResult);
+                System.out.println(thirdResult);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Retrieved all quotes in ");
+        });
     }
 }
