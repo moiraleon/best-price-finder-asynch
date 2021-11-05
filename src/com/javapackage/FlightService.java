636838724c9444e516f069c1328bb51811fe960d
Moira Leon <moira.leon2@gmail.com>
2021-11-04 18:21:52 -0600

    reteiving multiple quotes

--- src/com/javapackage/FlightService.java
+++ src/com/javapackage/FlightService.java
@@ -1,9 +1,19 @@
 package com.javapackage;
 
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FlightService  {
+    public Stream<CompletableFuture<Quote>> getQuotes(){
+        var sites = List.of("site 1", "site 2", "site 3");
+
+        return sites.stream()
+                .map(this::getQuote);
+    }
+
     public CompletableFuture<Quote>  getQuote (String site){
         return CompletableFuture.supplyAsync(()->{
 
--- src/com/javapackage/Main.java
+++ src/com/javapackage/Main.java
@@ -2,6 +2,7 @@ package com.javapackage;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -9,46 +10,16 @@ public class Main {
    public static void main(String[] args) {
 
        var service = new FlightService();
-       service.getQuote("site 1")
-               .thenAccept(System.out::println);
+//       service.getQuote("site 1")
+//               .thenAccept(System.out::println);
+       service.getQuotes()
+               .map(future -> future.thenAccept(System.out::println))
+               .collect(Collectors.toList());
 
        try {
            Thread.sleep(2000);
        } catch (InterruptedException e) {
 
        }
-
-
-//
-//        var site1 = CompletableFuture.supplyAsync(()->{
-//            return 1;
-//        });
-//
-//        var site2 = CompletableFuture.supplyAsync(()->{
-//            return 2;
-//        });
-//
-//        var site3 = CompletableFuture.supplyAsync(()->{
-//            return 3;
-//        });
-//
-//        var all = CompletableFuture.allOf(site1,site2,site3);
-//        all.thenRun(()->{
-//
-//            try {
-//                var firstResult = site1.get();
-//                var secondResult = site2.get();
-//                var thirdResult = site3.get();
-//                System.out.println(firstResult);
-//                System.out.println(secondResult);
-//                System.out.println(thirdResult);
-//
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            } catch (ExecutionException e) {
-//                e.printStackTrace();
-//            }
-//            System.out.println("Retrieved all quotes in ");
-//        });
    }
 }
