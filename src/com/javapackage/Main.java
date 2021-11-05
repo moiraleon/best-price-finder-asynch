2fd823b9d0b07908a0cbb31eb5d80abb2fe55245
Moira Leon <moira.leon2@gmail.com>
2021-11-04 18:33:52 -0600

    retreival time message

--- src/com/javapackage/Main.java
+++ src/com/javapackage/Main.java
@@ -1,5 +1,8 @@
 package com.javapackage;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
@@ -8,13 +11,20 @@ public class Main {
 
 
    public static void main(String[] args) {
+       var start = LocalTime.now();
 
        var service = new FlightService();
-//       service.getQuote("site 1")
-//               .thenAccept(System.out::println);
-       service.getQuotes()
+      var futures = service.getQuotes()
                .map(future -> future.thenAccept(System.out::println))
                .collect(Collectors.toList());
+
+      CompletableFuture
+              .allOf(futures.toArray(new CompletableFuture[0]))
+              .thenRun(()-> {
+                  var end = LocalTime.now();
+                  var duration = Duration.between(start,end);
+                  System.out.println("Retieved all quotes in "+duration.toMillis()+ " milliseconds.");
+              });
 
        try {
            Thread.sleep(2000);
